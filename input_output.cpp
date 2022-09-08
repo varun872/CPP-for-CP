e51f91f415edda5cb6b43653faa40c7495e6310c
varun872 <varun.hramurs@gmail.com>
2022-09-09 00:33:08 +0530

    added code for input_output

---
+++ input_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Single input
+    int a;
+    cin>>a;
+    cout<<"You entered: "<<a<<endl;
+    // Multiple input
+    int x,y;
+    cin>>x>>y;
+    cout<<"Addition of the 2 values is: "<<x+y<<endl;
+    return 0;
+}
