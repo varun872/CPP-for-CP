2e447e3bff2eda7c8e3493370e54a4f574524d44
varun872 <varun.hramurs@gmail.com>
2022-09-09 13:07:16 +0530

    added code to check if a number is prime or not

--- Practice/prime.cpp
+++ Practice/prime.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
     int n;
     cin>>n;
     int count=0;
-    for(int i=2;i<n;i++)
+    for(int i=2;i<=sqrt(n);i++)
     {
         if(n%i==0)
         {
