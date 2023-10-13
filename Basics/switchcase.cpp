45c699091ac8f46051902383b58380e9d12bda0e
varunhr <varun.hramurs@gmail.com>
2023-10-13 11:31:20 +0530

    Added basic c++ syntax codes to Basics file

--- Basics/switchcase.cpp
+++ Basics/switchcase.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    // simple calculator
-    int a,b;
-    cin>>a>>b;
-    char oper;
-    cin>>oper;
-    switch(oper)
-    {
-        case '+':
-            cout<<"The addition of the numbers is: "<<a+b<<endl;
-            break;
-        case '-':
-            cout<<"The difference of the numbers is: "<<a-b<<endl;
-            break;
-        case '*':
-            cout<<"The product of the numbers is: "<<a*b<<endl;
-            break;
-        case '/':
-            cout<<"The quotient is: "<<a/b<<endl;
-            break;
-        case '%':
-            cout<<"The remainder is: "<<a%b<<endl;
-            break;
-        default:
-            cout<<"Invalid operator!!"<<endl;
-            break;
-    }
-    return 0;
+int main() {
+  // simple calculator
+  int a, b;
+  cin >> a >> b;
+  char oper;
+  cin >> oper;
+  switch (oper) {
+  case '+':
+    cout << "The addition of the numbers is: " << a + b << endl;
+    break;
+  case '-':
+    cout << "The difference of the numbers is: " << a - b << endl;
+    break;
+  case '*':
+    cout << "The product of the numbers is: " << a * b << endl;
+    break;
+  case '/':
+    cout << "The quotient is: " << a / b << endl;
+    break;
+  case '%':
+    cout << "The remainder is: " << a % b << endl;
+    break;
+  default:
+    cout << "Invalid operator!!" << endl;
+    break;
+  }
+  return 0;
 }
